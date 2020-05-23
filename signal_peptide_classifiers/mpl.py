d2fd86969b3730b17825535bbecf7b0090f7f80e
valerie verhalle <v.verhalle@kpnmail.nl>
2020-05-23 11:38:55 +0200

    mlp code done

--- signal_peptide_classifiers/mpl.py
+++ signal_peptide_classifiers/mpl.py
@@ -1,34 +1,47 @@
 import preprocess_data
 import matplotlib.pyplot as plt
+from pandas import np
 from sklearn.metrics import plot_confusion_matrix
 from sklearn.neural_network import MLPClassifier
 
-instances_aa, instances_characteristics, class_ids = preprocess_data.parse_file()
-one_hots = preprocess_data.preprocessing(instances_aa)
-seq_train, seq_test, class_ids_train, class_ids_test = preprocess_data.split_data(one_hots, class_ids)
 
-#print(seq_train)
-# print(class_ids_train)
-#print(instances_aa)
+class MultiLayerPerception:
 
-X = [[0., 0.], [1., 1.]]
-y = [0, 1]
-# clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(15,), random_state=1)
+    def __init__(self):
+        self.seq_train = np.array
+        self.seq_test = np.array
+        self.class_ids_train = list
+        self.class_ids_test = list
 
-classifier = clf.fit(seq_train, class_ids_train)
+        self.mlp_classifier = MultiLayerPerception
 
-titles_options = [("Confusion matrix, without normalization", None), ("Normalized confusion matrix", 'true')]
+        self.titles_options = [("Confusion matrix, without normalization", None), ("Normalized confusion matrix", 'true')]
 
-for title, normalize in titles_options:
-    disp = plot_confusion_matrix(classifier, seq_test, class_ids_test,
-                                 display_labels=['SP', 'NO SP'],
-                                 cmap=plt.cm.Blues,
-                                 normalize=normalize)
-    disp.figure_.suptitle(title, fontsize=14, verticalalignment='center')
-    disp.ax_.set_title(f"Model: {'MLP'}, Data: aminoacid {'identities'}", fontsize=10)
+    def pre_process_data(self):
 
-    print(title)
-    print(disp.confusion_matrix)
+        instances_aa, instances_characteristics, class_ids = preprocess_data.parse_file()
+        one_hots = preprocess_data.preprocessing(instances_aa)
+        self.seq_train, self.seq_test, self.class_ids_train, self.class_ids_test = preprocess_data.split_data(one_hots, class_ids)
 
-    plt.show()
+    def classifier(self):
+
+        clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(15,), random_state=1)
+        self.mlp_classifier = clf.fit(self.seq_train, self.class_ids_train)
+
+    def confusion_matrix(self):
+
+        for title, normalize in self.titles_options:
+            disp = plot_confusion_matrix(self.mlp_classifier, self.seq_test, self.class_ids_test,
+                                         display_labels=['SP', 'NO SP'],
+                                         cmap=plt.cm.Blues,
+                                         normalize=normalize)
+            disp.figure_.suptitle(title, fontsize=14, verticalalignment='center')
+            disp.ax_.set_title(f"Model: {'MLP'}, Data: aminoacid {'identities'}", fontsize=10)
+
+            plt.show()
+
+
+mlp = MultiLayerPerception()
+mlp.pre_process_data()
+mlp.classifier()
+mlp.confusion_matrix()
